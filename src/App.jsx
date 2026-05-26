4b9ffe39c356742a9b49fa6965ee583683806086
Duamemon51 <duamemon51@gmail.com>
2026-05-26 12:56:28 +0500

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from './pages/home'
 import LegoraLanding from './pages/LegoraLanding'
 import Expertis from './pages/expertis'
 import Kundcase from './pages/Kundcase'
