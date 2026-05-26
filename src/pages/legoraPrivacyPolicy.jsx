de62ec8f09f2cbf5d1f04463f580876194c77d32
Duamemon51 <duamemon51@gmail.com>
2026-05-26 19:17:30 +0500

    nav

--- src/pages/legoraPrivacyPolicy.jsx
+++ src/pages/legoraPrivacyPolicy.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Navbar from "../components/navbar";
-import Footer from "../components/footer";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const SECTIONS = [
   {
